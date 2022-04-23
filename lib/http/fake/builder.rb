06a4024b4ab4a2cb30ab7f51c841177f743832b7
Brooke Kuhlmann <brooke@alchemists.io>
2022-04-23 09:14:46 -0600

    Added builder
    Necessary to build fake requests and responses and is the core for
    handling all fake exchanges. This will soon be used by the client.

---
+++ lib/http/fake/builder.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "uri"
+require "refinements/hashes"
+
+module HTTP
+  module Fake
+    # Builds the response for a request.
+    class Builder
+      using Refinements::Hashes
+
+      ALLOWED_VERBS = /\A(connect|delete|get|head|options|patch|post|put|trace)\z/
+
+      attr_reader :requests
+
+      # :reek:DuplicateMethodCall
+      def initialize verbs: ALLOWED_VERBS, responder: Responder
+        @verbs = verbs
+        @responder = responder
+        @requests ||= Hash.with_default Hash.with_default([])
+        @responders = Hash.with_default []
+      end
+
+      def request verb, uri, arguments = {}
+        responder = responders[verb].find { |fake| fake.match? verb, URI(uri).path }
+        missing_route_error verb, uri unless responder
+        requests[uri][verb] << arguments
+        responder.call uri, arguments
+      end
+
+      def method_missing(verb, *arguments, &)
+        respond_to_missing?(verb) ? add_responder(verb, *arguments, &) : super
+      end
+
+      private
+
+      attr_reader :verbs, :responder, :responders
+
+      def respond_to_missing?(verb, include_private = false) = verbs.match?(verb) || super
+
+      def add_responder verb, pattern, &block
+        responder.new(verb, pattern, block).then do |instance|
+          responders[[verb, pattern]].append instance
+        end
+      end
+
+      def missing_route_error verb, uri
+        fail StandardError, "No route for #{verb.upcase} #{uri} in responders: #{responders}."
+      end
+    end
+  end
+end
---
+++ spec/lib/http/fake/builder_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe HTTP::Fake::Builder do
+  subject(:builder) { described_class.new }
+
+  shared_examples "a request" do |verb|
+    it "answers array of matching responders without block" do
+      responders = builder.public_send(verb, "/test").map(&:inspect)
+      expect(responders).to contain_exactly(%r(pattern.+/test.+function=nil))
+    end
+
+    it "answers array of matching responders with block" do
+      responders = builder.public_send(verb, "/test") { "test" }
+                          .map(&:inspect)
+
+      expect(responders).to contain_exactly(%r(pattern.+/test.+function.+builder_spec))
+    end
+  end
+
+  describe "#connect" do
+    it_behaves_like "a request", :connect
+  end
+
+  describe "#delete" do
+    it_behaves_like "a request", :delete
+  end
+
+  describe "#get" do
+    it_behaves_like "a request", :get
+  end
+
+  describe "#head" do
+    it_behaves_like "a request", :head
+  end
+
+  describe "#options" do
+    it_behaves_like "a request", :options
+  end
+
+  describe "#patch" do
+    it_behaves_like "a request", :patch
+  end
+
+  describe "#post" do
+    it_behaves_like "a request", :post
+  end
+
+  describe "#put" do
+    it_behaves_like "a request", :put
+  end
+
+  describe "#trace" do
+    it_behaves_like "a request", :trace
+  end
+
+  describe "#request" do
+    it "answers HTTP response when responders exist" do
+      builder.get("/products") { "test" }
+      expect(builder.request(:get, "/products")).to be_a(HTTP::Response)
+    end
+
+    it "fails without any responders" do
+      expectation = proc { builder.request "get", "/products" }
+
+      expect(&expectation).to raise_error(
+        StandardError,
+        %(No route for GET /products in responders: {"get"=>[]}.)
+      )
+    end
+  end
+
+  describe "requests" do
+    it "answers empty hash with nothing registered" do
+      expect(builder.requests).to eq({})
+    end
+
+    it "answers request with no options when registered" do
+      builder.get("/products") { "test" }
+      builder.request :get, "/products"
+
+      expect(builder.requests).to eq("/products" => {get: [{}]})
+    end
+
+    it "answers request with options when registered" do
+      builder.get("/products") { "test" }
+      builder.request :get, "/products", format: :json
+
+      expect(builder.requests).to eq("/products" => {get: [{format: :json}]})
+    end
+  end
+end
